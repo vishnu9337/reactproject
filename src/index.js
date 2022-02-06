ce1a07c383ecfea025ed9703beb02c85ed4f1170
vishnu9337 <vishnusingh3886@gmail.com>
2022-02-06 08:13:38 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <>
+   <h1> netflix pick </h1>
+  <p> Here the list of my fav 5 netflix series</p>
+   <ol>
+     <li>dark</li>
+     <li>extra curricular</li>
+     <li>my holo love</li>
+     <li>my first -2 love</li>
+     <li>mr robot</li>
+   </ol> </>,
+  document.getElementById("root"));
+
+
